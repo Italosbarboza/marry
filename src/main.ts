e17c99ccccea69c31b237fe953f6ebc24e9fd284
Italo <italo_barboza@hotmail.com>
2024-05-30 16:25:53 -0300

    agora vai

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cors({
-    origin: ['https://listaperfeita.com','http://localhost:3000', 'https://app.listaperfeita.com'],
+    origin: ['https://listaperfeita.com', 'http://localhost:3000', 'https://app.listaperfeita.com'],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Inclua 'OPTIONS' aqui
   }));
 
+  // Adicione um middleware para tratar solicitações OPTIONS
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+    } else {
+      next();
+    }
+  });
+  
   await app.listen(5002);
 }
 bootstrap();
