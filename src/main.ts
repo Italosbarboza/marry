3c7ae6146066a23f8be3818d6aca7403caffa471
Italo <italo_barboza@hotmail.com>
2024-05-30 16:04:45 -0300

    adicionando cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cors({
-    origin: ['http://localhost:3000', 'https://app.listaperfeita.com'],
+    origin: ['https://listaperfeita.com','http://localhost:3000', 'https://app.listaperfeita.com'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }));
